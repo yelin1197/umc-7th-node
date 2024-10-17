dbc8555b4b871fd4b315057d7104e9b54ddf40a0
yelin1197 <yelin1197@ewhain.net>
2024-10-17 20:18:19 +0900

    umc

---
+++ src/dtos/uer.dto.js
@@ -0,0 +1,15 @@
+
+export const bodyToUser = (body) => {
+    const birth = new Date(body.birth);
+  
+    return {
+      email: body.email,
+      name: body.name,
+      gender: body.gender,
+      birth,
+      address: body.address || "",
+      detailAddress: body.detailAddress || "",
+      phoneNumber: body.phoneNumber,
+      preferences: body.preferences,
+    };
+  };
