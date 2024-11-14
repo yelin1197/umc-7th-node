0f7346aab5b40a248f65c1fa72d360cc57af1231
YELIN <132627658+yelin1197@users.noreply.github.com>
2024-11-14 17:32:45 +0900

    Delete src directory

--- src/dtos/store.dto.js
+++
@@ -1,9 +0,0 @@
-
-export const responseFromReviews = (reviews) => {
-    return {
-      data: reviews,
-      pagination: {
-        cursor: reviews.length ? reviews[reviews.length - 1].id : null,
-      },
-    };
-  };
--- src/services/store.service.js
+++
@@ -1,5 +0,0 @@
-
-export const listStoreReviews = async (storeId) => {
-    const reviews = await getAllStoreReviews(storeId);
-    return responseFromReviews(reviews);
-  };
